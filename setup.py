9390d56e4af209a67639b947bbebcb9110552a65
m-matelski <m.z.matelski@gmail.com>
2022-02-24 18:44:01 +0100

    Added entry points and optional dependencies for CLI tool

--- setup.py
+++ setup.py
@@ -1,25 +1,37 @@
 import setuptools
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setuptools.setup(
     name='mdiff',
     packages=setuptools.find_packages(),
     version='0.0.1',
     license='MIT',
-    description='Compare sequences',
+    description='Sequence matcher with displacement detection.',
+    long_description=readme(),
     author='Mateusz Matelski',
     author_email='m.z.matelski@gmail.com',
     url='https://github.com/m-matelski/mdiff',
-    download_url = 'https://github.com/m-matelski/mdiff/archive/v0.0.1.tar.gz',
-    keywords = ['sql', 'diff', 'postgres', 'teradata'],
-    install_requires=[
-        'colorama==0.4.4'
-    ],
+    keywords=['sequence', 'diff', 'heckel', 'text'],
+    entry_points={
+        'console_scripts': ['mdiff=mdiff.cli:main'],
+    },
+    extras_require={
+        'cli': ['colorama==0.4.*', 'typer==0.4.*']
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
     ],
 )
