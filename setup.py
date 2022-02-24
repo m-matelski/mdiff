94d35ccb3e27263d085f183e49cb970495140618
m-matelski <m.z.matelski@gmail.com>
2022-02-24 19:51:24 +0100

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setuptools.setup(
     name='mdiff',
     packages=setuptools.find_packages(),
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='Sequence matcher with displacement detection.',
     long_description=readme(),
