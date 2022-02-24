cbfe5f7077c5b3fdce2e5e3b6ada27bae5aa7e71
m-matelski <m.z.matelski@gmail.com>
2022-02-24 18:44:40 +0100

    Created SequenceMatcher factory

---
+++ mdiff/seqmatch/utils.py
@@ -0,0 +1,26 @@
+from difflib import SequenceMatcher
+from typing import Literal, Type
+
+from mdiff.seqmatch.heckel import HeckelSequenceMatcher, DisplacementSequenceMatcher
+from mdiff.utils import SequenceMatcherBase
+
+
+class SequenceMatcherName:
+    STANDARD = 'standard'
+    HECKEL = 'heckel'
+    DISPLACEMENT = 'displacement'
+
+
+t1 = Literal['a', 'b']
+seq_matchers = {
+    SequenceMatcherName.STANDARD: SequenceMatcher,
+    SequenceMatcherName.HECKEL: HeckelSequenceMatcher,
+    SequenceMatcherName.DISPLACEMENT: DisplacementSequenceMatcher,
+}
+
+
+def seq_matcher_factory(seq_matcher_type: str) -> Type[SequenceMatcherBase]:
+    values = SequenceMatcherName.__dict__.values()
+    if seq_matcher_type not in values:
+        raise ValueError(f'seq_matcher_type must be in: {values}')
+    return seq_matchers[seq_matcher_type]
