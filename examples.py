5e33f8cf087cc93075198aee1624721b4a7564e7
m-matelski <m.z.matelski@gmail.com>
2022-03-15 15:58:37 +0100

    Revert "added executable mode"
    This reverts commit fb828095

--- examples.py
+++
@@ -1,7 +0,0 @@
-from mdiff import HeckelSequenceMatcher
-
-a = ['line1', 'line2', 'line3', 'line4', 'line5']
-b = ['line1', 'line3', 'line2', 'line4', 'line6']
-sm = HeckelSequenceMatcher(a, b)
-for opcode in sm.get_opcodes():
-    print(opcode)
